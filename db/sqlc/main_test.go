4cbb4e250931fd9e9a87d9d02689704301c32b3e
Akshit Sadana <akshitsadana@gmail.com>
2021-01-16 16:49:38 +0530

    load config edit in MAIN_TEST

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -23,10 +23,7 @@ var testDB *sql.DB
 func TestMain(m *testing.M) {
 	var err error
 
-	config, err := util.LoadConfig("../../")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
+	config, _ := util.LoadConfig("../../")
 
 	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
